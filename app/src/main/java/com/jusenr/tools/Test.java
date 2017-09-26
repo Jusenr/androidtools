d7f66598d2e6562760221af0e8b8505bfc2abeb9
Jusenr <jusenr@163.com>
2017-09-27 00:04:28 +0800

    fix code 4

---
+++ app/src/main/java/com/jusenr/tools/Test.java
@@ -0,0 +1,21 @@
+package com.jusenr.tools;
+
+import com.jusenr.toolslibrary.utils.DateUtils;
+import com.jusenr.toolslibrary.utils.MD5Utils;
+
+/**
+ * Description:
+ * Author     : Jusenr
+ * Email      : jusenr@163.com
+ * Date       : 2017/09/18  22:30.
+ */
+
+public class Test {
+
+    public static void main(String[] args) {
+        String md5String = MD5Utils.getMD5String("Hello world!");
+        System.err.println(md5String);
+        String date = DateUtils.getCurrentDate();
+        System.out.println(date);
+    }
+}
--- bintrayUpload.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-./gradlew toolslibrary:clean
-
-./gradlew toolslibrary:build
-
-./gradlew bintrayUpload -PbintrayUser=jusenr -PbintrayKey=eac60de807dbad4fe6baedfb70b95fdf9ecaeb8d -PdryRun=false
